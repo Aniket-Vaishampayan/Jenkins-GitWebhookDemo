fc3009acd1204ac0084438bb049d477982976b6a
avisys-jenkins <jenkins.avisys@gmail.com>
2025-06-19 17:54:13 +0530

    I have remove and added a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { EmployeeComponent } from './components/forms/employee/employee.component';
 import { EmployeeTableComponent } from './components/forms/employee-table/employee-table.component';
 import { EmployeeService } from './services/employee.service';
-import { Test2Component } from './components/forms/test2/test2.component';
 import { Test3Component } from './components/forms/test3/test3.component';
 import { Test4Component } from './components/forms/test4/test4.component';
 import { Test5Component } from './components/forms/test5/test5.component';
+import { Test7Component } from './components/forms/test7/test7.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
     EmployeeTableComponent,
-    Test2Component,
     Test3Component,
     Test4Component,
-    Test5Component
+    Test5Component,
+    Test7Component
   ],
   imports: [
     BrowserModule,
