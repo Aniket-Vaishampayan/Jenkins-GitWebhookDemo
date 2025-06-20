9dda2b9c93ff1f9256c216627cef7c249ef97e6e
avisys-jenkins <jenkins.avisys@gmail.com>
2025-06-20 12:17:29 +0530

    I have added a new component test8

---
+++ src/app/components/forms/test8/test8.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test8',
+  templateUrl: './test8.component.html',
+  styleUrls: ['./test8.component.css']
+})
+export class Test8Component {
+
+}
