e9e1e0cd66267b3ac37172e101a0c09c9c85c4ca
avisys-jenkins <jenkins.avisys@gmail.com>
2025-06-20 12:13:34 +0530

    Remove component Test 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { EmployeeComponent } from './components/forms/employee/employee.component';
 import { EmployeeTableComponent } from './components/forms/employee-table/employee-table.component';
 import { EmployeeService } from './services/employee.service';
-import { Test3Component } from './components/forms/test3/test3.component';
 import { Test5Component } from './components/forms/test5/test5.component';
-import { Test7Component } from './components/forms/test7/test7.component';
 import { Test6Component } from './components/forms/test6/test6.component';
 
 @NgModule({
@@ -18,9 +16,7 @@ import { Test6Component } from './components/forms/test6/test6.component';
     AppComponent,
     EmployeeComponent,
     EmployeeTableComponent,
-    Test3Component,
     Test5Component,
-    Test7Component,
     Test6Component
   ],
   imports: [
