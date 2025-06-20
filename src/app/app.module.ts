1761fc968c3f6f16b8c847f392f90d8d5c414fdf
avisys-jenkins <jenkins.avisys@gmail.com>
2025-06-20 12:35:59 +0530

    I have added and remove component with add statement into the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,18 +6,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { EmployeeComponent } from './components/forms/employee/employee.component';
 import { EmployeeTableComponent } from './components/forms/employee-table/employee-table.component';
 import { EmployeeService } from './services/employee.service';
-import { Test5Component } from './components/forms/test5/test5.component';
 import { Test6Component } from './components/forms/test6/test6.component';
 import { Test8Component } from './components/forms/test8/test8.component';
+import { Test9Component } from './components/forms/test9/test9.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
     EmployeeTableComponent,
-    Test5Component,
     Test6Component,
-    Test8Component
+    Test8Component,
+    Test9Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/forms/test9/test9.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test9',
+  templateUrl: './test9.component.html',
+  styleUrls: ['./test9.component.css']
+})
+export class Test9Component {
+
+}
